068f48fce7c6745a1ede14cf0da0e92a64386e8f
Ben <bendelathouwer@users.noreply.github.com>
2023-05-21 15:51:34 +0200

    wraper continued
    scoop wrapper continued

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 import scoop_wrapper
-
+import time
 scoop=scoop_wrapper.scoop()
+scoop.setnrofavrages(2)
+avrages = scoop.nrofavrages()
+print(avrages)
--- src/scoop_wrapper.py
+++ src/scoop_wrapper.py
@@ -1,8 +1,9 @@
 import pyvisa
 import sys
+import time
 rm = pyvisa.ResourceManager()
 class scoop(object):
-    # TODO: error handeling beter op poten zetten
+    # TODO: error handeling beter op poten zetten+ visa instument list "variable maken
     def __init__(self):
         self.visaInstrList= rm.list_resources()
         print(self.visaInstrList)
@@ -20,5 +21,21 @@ class scoop(object):
         else:
             print("Identification string: '%s'" % idn_string)
         self.scope.timeout = 15000
-    def setchannels(self,channelnr,channelstatus):
-        pass
+    def autoscale(self):#autoscales the scoop trace on any channel
+        self.scope.write(":AUT")
+    def clearscoop(self):#clears the traces in single run mode from the display
+        self.scope.write(":CLE")
+    def scooprun(self):#sets the scope  in run mode
+        self.scope.write(":RUN")
+    def scoopstop(self):#sets the scope  in stop mode
+        self.scope.write(":STOP")
+    def singlecapture(self):
+        self.scope.write(":SING")
+    def forcetrigger(self):#only works in single or normal trigger mode
+        self.scope.write(":TFOR")
+    def nrofavrages(self):
+        nr=self.scope.query(":ACQ:AVER?")
+        return nr
+    def setnrofavrages(self,nr):#error 
+
+        self.scope.write(":ACQ:AVER"+str(nr))
