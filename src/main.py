0520e80d7a13714f1e8031e1cad9733e33e93ae2
Ben <bendelathouwer@users.noreply.github.com>
2023-05-28 21:13:57 +0200

    added the display command to the scoop libary

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ import scoop_wrapper
 import time
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.8::INSTR")
 channel=4
-scoop.setchannelcoupling(channel,"AC")
+scoop.setdisplaychannel(channel,1)
 time.sleep(5)
-scoop.setchannelcoupling(channel,"GND")
+scoop.setdisplaychannel(channel,0)
 time.sleep(5)
-scoop.setchannelcoupling(channel,"DC")
+scoop.setdisplaychannel(channel,"ON")
+time.sleep(5)
+scoop.setdisplaychannel(channel,"OFF")
