3a58a85acd488b8d37e63de7954477ef89f9cd26
bendelathouwer <bendelathouwer@gmail.com>
2025-01-04 18:46:10 +0100

    plotting needs figuering out but atleast getting data from the scoop

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 
 import scoop_wrapper
+import matplotlib.pyplot as plt
+#plotting needs figuering out the plotting
 data= []
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.8::INSTR")
 data = scoop.takemeasurement("CHAN1","NORMal","ASCii")
 print(data)
 print(type(data))
+print(len(data))
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+
+
+for i in range (len(data)):
+    ax.plot(i, data)
+plt.show()
