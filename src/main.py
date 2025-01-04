08bb3e79b1d6d2608d999a551cdbfa65eef5b5d4
bendelathouwer <bendelathouwer@gmail.com>
2025-01-04 18:35:31 +0100

    got the shit working again

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
-import scoop_wrapper  
-import time  
+
+import scoop_wrapper
+data= []
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.8::INSTR")
+data = scoop.takemeasurement("CHAN1","NORMal","ASCii")
+print(data)
+print(type(data))
