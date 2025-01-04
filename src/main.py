d7aaafaf77537d1485bafc352cc4e16f5b8d5fd0
bendelathouwer <bendelathouwer@gmail.com>
2025-01-04 18:54:48 +0100

    plotting kinda works but not yet the way i like it

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 
 import scoop_wrapper
 import matplotlib.pyplot as plt
+import numpy as np
 #plotting needs figuering out the plotting
-data= []
+
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.8::INSTR")
 data = scoop.takemeasurement("CHAN1","NORMal","ASCii")
 print(data)
 print(type(data))
 print(len(data))
+t = np.arange(0, len(data))
 fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-
-
-for i in range (len(data)):
-    ax.plot(i, data)
+ax.plot([t], [data])  # Plot some data on the Axes.
 plt.show()
+
