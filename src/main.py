4e05563fe75b77e0a88f7c7d9e389449d31b2362
bendelathouwer <bendelathouwer@gmail.com>
2025-02-21 18:43:02 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,5 +6,5 @@ import numpy as np
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")
 data = scoop.takemeasurement("CHAN1","NORMal","ASCii")
 
-plt.plot([len(data)],[data])
+plt.plot(100,data)
 plt.show()
