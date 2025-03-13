80e08591312b2314fbb4f58aea85b71c7c0ebbd7
bendelathouwer <bendelathouwer@gmail.com>
2025-03-13 19:04:05 +0100

    fuck around find out
    still fucking around and finding out

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")
-x,data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
+data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
 index= np.nditer(data)
-plt.plot(x,data)
+plt.plot(data,len(index))
 plt.show()
