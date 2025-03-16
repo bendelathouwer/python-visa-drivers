2dd1b925833a1ff35086a77a73cfe253324ba0f8
bendelathouwer <bendelathouwer@gmail.com>
2025-03-16 09:45:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ import scoop_wrapper
 import matplotlib.pyplot as plt
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")
 data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
-plt.plot(data)#plt.plot takes an 1d np array and iterates over it by itself , no need to pass y values
+plt.plot(data,linestyle= '--')#plt.plot takes an 1d np array and iterates over it by itself , no need to pass y values
 plt.show()
