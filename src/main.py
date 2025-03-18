2a11623ad2db3e12eb7858493d204a6e9572826d
bendelathouwer <bendelathouwer@gmail.com>
2025-03-18 12:04:04 +0100

    added sql demmo
    added sql dome file

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@
 import scoop_wrapper
 import matplotlib.pyplot as plt
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")
-data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
-plt.plot(data,linestyle= '--')#plt.plot takes an 1d np array and iterates over it by itself , no need to pass y values
+data= scoop.takemeasurement(" CHAN2","NORMal","ASCii")
+plt.plot(data,marker='')#plt.plot takes an 1d np array and iterates over it by itself , no need to pass y values
 plt.show()
