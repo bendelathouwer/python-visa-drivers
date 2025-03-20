ee6254e9dcae2358b464cc7fc7a06b6a08eb3bb3
bendelathouwer <bendelathouwer@gmail.com>
2025-03-20 11:10:11 +0100

    Update sqldemo.py

--- src/sqldemo.py
+++ src/sqldemo.py
@@ -1,9 +1,9 @@
 import sqlalchemy as db
-from sqlalchemy import Table, Column, Integer, String, MetaData,Float
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import *
+from sqlalchemy_utils import *
 import scoop_wrapper
 import matplotlib.pyplot as plt
-from sqlalchemy_utils import database_exists, create_database
+
 
 #creates a connection and if DB does not exist it creats it
 engine = db.create_engine("mysql+pymysql://root:@localhost/rigoldb")#pip install pymysql
@@ -14,10 +14,12 @@ connection = engine.connect()
 
 #creates table with the folowing "settings", any variable settings (float,int etc needs to be imported form the lib )
 test_table=Table("scope",meta,Column("id", Integer, primary_key=True),
+Column("sample", Integer),
 Column("voltage", Float))
 meta.create_all(engine)
 
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")#connects to the scope over lan using these values
 data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
+
 plt.plot(data)
 plt.show()
