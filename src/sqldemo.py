a44ce1926837cf338a3d5c6bc2e1e144af931a99
bendelathouwer <bendelathouwer@gmail.com>
2025-03-20 10:59:52 +0100

    Update sqldemo.py

--- src/sqldemo.py
+++ src/sqldemo.py
@@ -1,6 +1,6 @@
 import sqlalchemy as db
-from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, String, MetaData,Float
+from sqlalchemy_utils import database_exists, create_database
 import scoop_wrapper
 import matplotlib.pyplot as plt
 from sqlalchemy_utils import database_exists, create_database
@@ -11,9 +11,10 @@ meta = MetaData()
 if not database_exists(engine.url):
     create_database(engine.url)
 connection = engine.connect()
-#creates table with the folowing "settings"
+
+#creates table with the folowing "settings", any variable settings (float,int etc needs to be imported form the lib )
 test_table=Table("scope",meta,Column("id", Integer, primary_key=True),
-Column("voltage", Integer))
+Column("voltage", Float))
 meta.create_all(engine)
 
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")#connects to the scope over lan using these values
