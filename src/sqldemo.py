52294cccb2bb527d0e7c06857657db523fba840d
bendelathouwer <bendelathouwer@gmail.com>
2025-03-24 14:38:04 +0100

    Update sqldemo.py

--- src/sqldemo.py
+++ src/sqldemo.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 #creates a connection and if DB does not exist it creats it
-engine = db.create_engine("mysql+pymysql://root:@localhost/rigoldb")#pip install pymysql
+engine = db.create_engine("mysql+pymysql://root:@localhost/rigoldb")#pip install pymysql otherwise this will not work
 meta = MetaData()
 if not database_exists(engine.url):
     create_database(engine.url)
@@ -14,11 +14,12 @@ connection = engine.connect()
 
 #creates table with the folowing "settings", any variable settings (float,int etc needs to be imported form the lib )
 test_table=Table("scope",meta,Column("id", Integer, primary_key=True),
-Column("voltage", Float))
+                 Column("voltage", Float))
 meta.create_all(engine)
 
 scoop=scoop_wrapper.scoop("TCPIP0::169.254.226.9::INSTR")#connects to the scope over lan using these values
 data= scoop.takemeasurement("CHAN1","NORMal","ASCii")
+
 #inserts data in the database from the measurments
 for i in range(len(data)):
     stmt =  insert(test_table).values(voltage = data[i])
